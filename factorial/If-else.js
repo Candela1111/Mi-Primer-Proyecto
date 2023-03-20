b3d4270f55da14cdcb83f899bf891fdaf35dec14
Candela1111 <97445151+Candela1111@users.noreply.github.com>
2023-03-20 18:06:15 +0100

    Add files via upload

---
+++ factorial/If-else.js
@@ -0,0 +1,54 @@
+//Bifurcaciones If Else
+/*let saldo= 50;
+let efectivo=20;
+
+if(efectivo<saldo){
+    console.log("Puedes sacar dinero")
+}else{
+    console.log("Saldo insuficiente")
+}*/
+
+// if.. else+ if.. else
+
+/*let nota=5;
+
+if(nota===5){
+    console.log("Enhorabuena has obtenido un sobresaliente")
+}else if(nota===4){
+    console.log("Buen trabajo pero podrias hacerlo mejor!")
+}else if(nota===3){
+    console.log("La proxima vez puedes esforzarte un poco más")
+}else if(nota===2){
+    console.log("Rozando el suspenso")
+}else if(nota===1){
+    console.log("Vaya, vaya estas supenso..")
+}else{
+    console.log("Error, introduce una nota del 1 al 5")
+}*/
+
+// Switch 
+/*let nota=4;
+
+switch(nota){
+    case 5: 
+    console.log("Buen trabajo, tienes un sobresaliente")
+    break;
+    case 4: 
+    console.log("Buen trabajo la proxima mejor")
+    break;
+    case 3: 
+    console.log("Puedes hacerlo mejor")
+    break;
+    case 2: 
+    console.log("Estudia más")
+    break;
+    case 1: 
+    console.log("Vaya,vaya has suspendido..")
+    break;
+    default:
+        console.log("Error")
+        break;
+}*/
+
+
+
