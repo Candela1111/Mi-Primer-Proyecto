2fe8e52e448fc2dceb2eb67880e4bd2256a86e5c
Candela1111 <97445151+Candela1111@users.noreply.github.com>
2023-03-28 16:49:54 +0200

    Add files via upload

---
+++ factorial/ejercicioStrings.js
@@ -0,0 +1,13 @@
+let nombre ="Candela";
+let apellido ="Cobo";
+
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = estudiante.toUpperCase;
+let estudianteMinus = estudiante.toLowerCase;
+let numeroestudiante = estudiante.length; 
+let letranombre = nombre.charAt(0);
+let letraapellido = apellido[3];
+let estudiantesin = estudiante.replace(" ", "");
+console.log(estudiantesin);
+let existe = nombre.includes("Candela");
+console.log(existe);
