39da369ff43b3529803db8a03fe263db073b15dd
Candela1111 <97445151+Candela1111@users.noreply.github.com>
2023-04-03 19:06:09 +0200

    Add files via upload

---
+++ factorial/ejercicioNumeros.js
@@ -0,0 +1,9 @@
+let altura = 167;
+let alturaf = 1.67;
+let peso = 63.3;
+let alturar = Math.ceil(alturaf);
+console.log(alturar);
+let pesor = Math.floor(peso);
+console.log(pesor);
+let maxv= Number.MAX_VALUE + 1;
+console.log(maxv); 
